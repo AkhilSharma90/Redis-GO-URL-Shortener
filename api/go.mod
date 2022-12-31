96ec6adc289f0b76cb3bbefcbe8bfb4e7fafe373
akhil <akhilsails@gmail.com>
2022-12-31 08:57:07 +0530

    working code

---
+++ api/go.mod
@@ -0,0 +1,11 @@
+module github.com/akhil/fiber-url-shortener
+
+go 1.16
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/go-redis/redis/v8 v8.11.4
+	github.com/gofiber/fiber/v2 v2.24.0
+	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.4.0
+)
---
+++ api/routes/resolve.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"github.com/akhil/fiber-url-shortener/database"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+)
+
+// ResolveURL ...
+func ResolveURL(c *fiber.Ctx) error {
+	// get the short from the url
+	url := c.Params("url")
+	// query the db to find the original URL, if a match is found
+	// increment the redirect counter and redirect to the original URL
+	// else return error message
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found on database",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to DB",
+		})
+	}
+	// increment the counter
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
+	// redirect to original URL
+	return c.Redirect(value, 301)
+}
